4c5f01fa0d02bf0ea2ffdb9447733d8538e2228a
Delermando <d.santos@personare.com.br>
2016-07-20 11:03:05 -0300

    create load test definition file

---
+++ src/load_test_definition.py
@@ -0,0 +1,12 @@
+from locust import TaskSet, task, HttpLocust
+
+class Tasks(TaskSet):
+    @task
+    def index(self):
+        self.client.get('/')
+
+class MyHostLoadTest(HttpLocust):
+    host = "http://iamyourcoder.com"
+    task_set = Tasks
+    min_wait = 2000
+    max_wait = 5000
